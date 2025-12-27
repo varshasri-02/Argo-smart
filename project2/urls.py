d47f59d37f81f5aeb342d61d9b728834bd4ad587
varshasri-02 <rvvarshasri@gmail.com>
2025-12-27 18:00:04 +0530

    fix: Enable static file serving in development

--- project2/urls.py
+++ project2/urls.py
@@ -63,3 +63,8 @@ urlpatterns = [
 
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+# Serve static files in development
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
